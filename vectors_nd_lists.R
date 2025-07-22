2ec8ed72e1d7fb35a262f6d0893ed9b8e588ba20
Harshit Jain <hj01032004@gmail.com>
2025-07-22 14:27:37 +0530

    Added Comments

--- vectors_nd_lists.R
+++ vectors_nd_lists.R
@@ -1,14 +1,14 @@
-xy <- 1:5
+xy <- 1:5 ## trying vectors
 xy
 class(xy)
 typeof(xy)
-as.numeric(xy)
+as.numeric(xy) ## explicit coercion of vectors
 class(xy)
 typeof(xy)
 as.logical(xy)
 class(xy)
 xy
-xy <- as.logical(xy)
+xy <- as.logical(xy) ##explicit coercion of vectors
 xy
 class(xy)
 as.character(xy)
@@ -17,10 +17,10 @@ print(xy)
 xy <- c(xy, 2, xy)
 xy
 
-zy <- list(1, "2", TRUE , xy , zy)
+zy <- list(1, "2", TRUE , xy , zy) ## trying lists
 zy
 class(zy)
 
-xy <- c(1, TRUE, FALSE, "a")
+xy <- c(1, TRUE, FALSE, "a") ## implicit coercion of vectors by taking the lcd of the classes
 xy
 class(xy)
